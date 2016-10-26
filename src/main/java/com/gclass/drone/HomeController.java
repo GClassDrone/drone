7770530e6c514c460487a49444d2c5da9abdac54
김지구 <sangdi84@hotmail.com>
2016-10-26 20:38:12 +0900

    인성씨가 지운 홈 컨트롤러 업데이트 합니다

---
+++ src/main/java/com/gclass/drone/HomeController.java
@@ -0,0 +1,20 @@
+package com.gclass.drone;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	@RequestMapping("/map")
+	public void map(){
+		
+	}
+	@RequestMapping("/Main")
+	public void main(){
+		
+	}
+	@RequestMapping("/CGlist")
+	public void cglist(){
+		
+	}
+}
