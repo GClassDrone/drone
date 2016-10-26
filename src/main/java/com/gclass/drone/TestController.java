bfc979f28528df3029167c3793730897ef577b28
최봉석 <sysero@nate.com>
2016-10-26 12:35:22 +0900

    jjjk

--- src/main/java/com/gclass/drone/TestController.java
+++ src/main/java/com/gclass/drone/TestController.java
@@ -1,14 +1,18 @@
 package com.gclass.drone;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class TestController {
-
+	Logger logger = LoggerFactory.getLogger(TestController.class);
+	
 	@RequestMapping(value="/test", method=RequestMethod.GET)
 	public String test(){
+		logger.info("uptest");
 		return "test";
 	}
 }
