05bc3d412da20d6f84126837535190d74cfd5365
모인성 <ahdlstjd@naver.com>
2016-11-01 19:30:04 +0900

    프로필 관련 Dto,dao, mapper

---
+++ src/main/java/com/gclass/profile/dto/MemDto.java
@@ -0,0 +1,120 @@
+package com.gclass.profile.dto;
+
+import java.util.Date;
+
+public class MemDto {
+	private int mno;
+	private String niknm;
+	private int siteno;
+	private String email;
+	private String pic;
+	private String license;
+	private String msgrcyn;
+	private String prmsg;
+	private int regcnt;
+	private int datcnt;
+	private int choocnt;
+	private int favcnt;
+	private Date indt;
+	private int levno;
+	private int locno;
+	
+	public int getMno() {
+		return mno;
+	}
+	public void setMno(int mno) {
+		this.mno = mno;
+	}
+	public String getNiknm() {
+		return niknm;
+	}
+	public void setNiknm(String niknm) {
+		this.niknm = niknm;
+	}
+	public int getSiteno() {
+		return siteno;
+	}
+	public void setSiteno(int siteno) {
+		this.siteno = siteno;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPic() {
+		return pic;
+	}
+	public void setPic(String pic) {
+		this.pic = pic;
+	}
+	public String getLicense() {
+		return license;
+	}
+	public void setLicense(String license) {
+		this.license = license;
+	}
+	public String getMsgrcyn() {
+		return msgrcyn;
+	}
+	public void setMsgrcyn(String msgrcyn) {
+		this.msgrcyn = msgrcyn;
+	}
+	public String getPrmsg() {
+		return prmsg;
+	}
+	public void setPrmsg(String prmsg) {
+		this.prmsg = prmsg;
+	}
+	public int getRegcnt() {
+		return regcnt;
+	}
+	public void setRegcnt(int regcnt) {
+		this.regcnt = regcnt;
+	}
+	public int getDatcnt() {
+		return datcnt;
+	}
+	public void setDatcnt(int datcnt) {
+		this.datcnt = datcnt;
+	}
+	public int getChoocnt() {
+		return choocnt;
+	}
+	public void setChoocnt(int choocnt) {
+		this.choocnt = choocnt;
+	}
+	public int getFavcnt() {
+		return favcnt;
+	}
+	public void setFavcnt(int favcnt) {
+		this.favcnt = favcnt;
+	}
+	public Date getIndt() {
+		return indt;
+	}
+	public void setIndt(Date indt) {
+		this.indt = indt;
+	}
+	public int getLevno() {
+		return levno;
+	}
+	public void setLevno(int levno) {
+		this.levno = levno;
+	}
+	public int getLocno() {
+		return locno;
+	}
+	public void setLocno(int locno) {
+		this.locno = locno;
+	}
+	
+	@Override
+	public String toString() {
+		return "MemDto [mno=" + mno + ", niknm=" + niknm + ", siteno=" + siteno + ", email=" + email + ", pic=" + pic
+				+ ", license=" + license + ", msgrcyn=" + msgrcyn + ", prmsg=" + prmsg + ", regcnt=" + regcnt
+				+ ", datcnt=" + datcnt + ", choocnt=" + choocnt + ", favcnt=" + favcnt + ", indt=" + indt + ", levno="
+				+ levno + ", locno=" + locno + "]";
+	}
+}
