7dbfaa3819728ba06fcc7ff896308a7272987c7e
모인성 <ahdlstjd@naver.com>
2016-11-03 16:53:22 +0900

    댓글 수정중

--- src/main/java/com/gclass/drone/HomeController.java
+++ src/main/java/com/gclass/drone/HomeController.java
@@ -3,6 +3,7 @@ package com.gclass.drone;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping
 @Controller
 public class HomeController {
 	@RequestMapping("/map/map")
