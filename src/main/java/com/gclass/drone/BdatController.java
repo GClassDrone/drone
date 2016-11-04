7bab7d3943293b23832488fb30c485fa8b65b3b7
모인성 <ahdlstjd@naver.com>
2016-11-04 17:35:14 +0900

    ㅁㄴㅇㅁㄴㅇㅁㄴㅇ

---
+++ src/main/java/com/gclass/drone/BdatController.java
@@ -0,0 +1,13 @@
+package com.gclass.drone;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/bdat")
+@Controller
+public class BdatController {
+	@RequestMapping("/bdat")
+	public void map(){
+		
+	}
+}
