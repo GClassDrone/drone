6e5768df8be1dc87ec311c7c4eba324081ee325f
모인성 <ahdlstjd@naver.com>
2016-11-09 11:47:36 +0900

    멤 컨트롤러

--- src/main/java/com/gclass/drone/mem/controller/MemController.java
+++ src/main/java/com/gclass/drone/mem/controller/MemController.java
@@ -1,18 +1,30 @@
 package com.gclass.drone.mem.controller;
 
+import javax.inject.Inject;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@RequestMapping
+import com.gclass.drone.mem.service.MemService;
+
+
+@RequestMapping("/profile")
 @Controller
 public class MemController {
-	@RequestMapping("/profile/ProfileDetail")
-	public void map() {
-
+	
+	@Inject
+	private MemService service;
+	
+	@RequestMapping(value="/ProfileDetail", method=RequestMethod.GET)
+	public void read(@RequestParam("mno") int mno, Model model) throws Exception {
+		
+		model.addAttribute(service.read(mno));
 	}
 
-	@RequestMapping("/profile/ProfileList")
+	@RequestMapping("/ProfileList")
 	public void main() {
 	}
 }
